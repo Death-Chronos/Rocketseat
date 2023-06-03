53abd7e0a510c3a994af85983ef4ffcd039b9630
João Victor Costa <80175567+Death-Chronos@users.noreply.github.com>
2023-06-03 20:51:50 -0300

    Simbora

---
+++ voto.js
@@ -0,0 +1,11 @@
+let idade = 16; 
+
+
+if (idade>=18&&idade<=69) {
+    console.log("Voto obrigatório: " + idade + " Anos");
+} else if(idade>15||idade>69){
+    console.log("Voto opicional: " + idade + " Anos");
+}
+else{
+    console.log("Não pode votar: " + idade + " Anos");
+}
